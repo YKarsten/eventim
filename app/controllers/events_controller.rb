8e5f0da6153736d6ef8f111afc2ca63427b7330c
YKarsten <yannik.karsten@web.de>
2023-04-28 13:18:10 +0200

    Add edit and update methods

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,50 @@
+class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    @user = @event.host
+  end
+
+  def new
+    @user = current_user
+    @event = Event.new
+  end
+
+  def create
+    @user = current_user
+    @event = @user.hosted_events.build(event_params)
+    if @event.save
+      @event.attendances.create(user: current_user)
+      redirect_to root_url
+    else
+      flash.now[:errors] = @event.errors.full_messages
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      flash.now[:errors] = @event.errors.full_messages
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:event_name, :description, :location, :date, :host_id)
+  end
+end
