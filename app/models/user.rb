282117ff7ddd455e6db1767e383eb16d8834f68b
YKarsten <yannik.karsten@web.de>
2023-04-28 13:34:37 +0200

    Set association with foreign key and through table, validation and devise authentification

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  has_many :hosted_events, class_name: 'Event', foreign_key: 'host_id', dependent: :destroy
+  has_many :attendances, dependent: :destroy
+  has_many :attend_events, through: :attendances, source: :event
+
+  validates :username, presence: true, uniqueness: true
+  validates :display_name, presence: true, uniqueness: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
