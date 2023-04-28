5568c3d2b4eb0e52c31c169dda9bebc6d878b99a
YKarsten <yannik.karsten@web.de>
2023-04-28 13:13:09 +0200

    Add 'devise' for users; set resources for events, users and 'through table' attendances

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'events#index'
+
+  resources :events, only: %i[index show new create edit update] do
+    resources :attendances, only: %i[create destroy]
+  end
+  resources :users
 end
